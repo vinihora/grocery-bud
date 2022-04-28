cacdd3d06fbbcd53baf3fd2cc7c4b081a0416b5e
Vinicius Hora <vinidahora08@hotmail.com>
2022-04-28 17:23:37 -0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import "./App.css";
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import { AiOutlineEdit } from "react-icons/ai";
 import { FaRegTrashAlt } from "react-icons/fa";
+
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [actualTask, setActualTask] = useState("");
   const [edit, setEdit] = useState(false);
   const [id, setId] = useState("");
+  const [message, setMessage] = useState("");
+  const [color, setColor] = useState("");
+  const [show, setShow] = useState("none");
+
+  const clearAll = () => {
+    setTasks([]);
+    setMessage("Empty List");
+    setColor("#ffd4dc");
+  };
 
   const addTask = () => {
+    if (actualTask === "") {
+      setMessage("Please Enter Value");
+      setColor("#ffd4dc");
+      return;
+    }
     if (edit) {
       tasks[id] = actualTask;
       setTasks([...tasks]);
       setEdit(false);
+      setMessage("Value Changed");
+      setColor("#d8ecdc");
       return;
     }
+    setMessage("Item Added To The List");
+    setColor("#d8ecdc");
+    setActualTask("");
     setTasks([...tasks, actualTask]);
   };
 
@@ -25,6 +45,8 @@ const App = () => {
 
   const excludeTask = (count) => {
     tasks.splice(count, 1);
+    setMessage("Item Removed");
+    setColor("#ffd4dc");
     setTasks([...tasks]);
   };
 
@@ -34,9 +56,28 @@ const App = () => {
     setEdit(true);
   };
 
+  useEffect(() => {
+    console.log(tasks.length);
+    if (tasks.length !== 0) {
+      return setShow("flex");
+    }
+    setShow("none");
+  }, [tasks]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setMessage("");
+      setColor("");
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [message]);
+
   return (
     <section className="body">
       <div className="card">
+        <div className="notification" style={{ background: color }}>
+          {message}
+        </div>
         <h1>Grocery Bud</h1>
         <div className="search-line">
           <input
@@ -46,7 +87,7 @@ const App = () => {
             id="searchInput"
             value={actualTask}
           ></input>
-          <button className="btn" onClick={() => addTask()}>
+          <button className="btn" onClick={() => addTask()} type="submit">
             <h5>Submit</h5>
           </button>
         </div>
@@ -71,6 +112,15 @@ const App = () => {
             );
           })}
         </li>
+        <div class="clear">
+          <button
+            class="btn-warn"
+            style={{ display: show }}
+            onClick={() => clearAll()}
+          >
+            <h4>Clear Items</h4>
+          </button>
+        </div>
       </div>
     </section>
   );
